4db25e1c1763ed4edd27cc54859964c2e4b8d62f
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-25 20:18:04 +0200

    Activity Log

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -4,9 +4,19 @@ namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
 
-use App\Policies\{CompanyPolicyMain, HardwarePolicy, PeriphelPolicy, ProvaiderPolicy, SoftwarePolicy};
-use App\Models\{Company, Hardware, Periphel, Provaider, Software};
+use App\Models\Company;
+use App\Models\Hardware;
+use App\Models\Periphel;
+use App\Models\Provaider;
+use App\Models\Software;
+use App\Policies\ActivityPolicy;
+use App\Policies\CompanyPolicyMain;
+use App\Policies\HardwarePolicy;
+use App\Policies\PeriphelPolicy;
+use App\Policies\ProvaiderPolicy;
+use App\Policies\SoftwarePolicy;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
+use Spatie\Activitylog\Models\Activity;
 
 class AuthServiceProvider extends ServiceProvider
 {
@@ -21,6 +31,7 @@ class AuthServiceProvider extends ServiceProvider
         PeriphelPolicy::class => Periphel::class,
         ProvaiderPolicy::class => Provaider::class,
         SoftwarePolicy::class => Software::class,
+        // Activity::class => ActivityPolicy::class,
     ];
 
     /**
