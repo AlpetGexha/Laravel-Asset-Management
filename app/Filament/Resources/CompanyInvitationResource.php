9c1464976eefb11c648f4ff3b517eab1b04b99d9
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-25 19:34:44 +0200

    Make Company Employee Invitation

---
+++ app/Filament/Resources/CompanyInvitationResource.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CompanyInvitationResource\Pages;
+use App\Filament\Resources\CompanyInvitationResource\RelationManagers;
+use App\Models\CompanyInvitation;
+use Filament\Forms;
+use Filament\Resources\Form;
+use Filament\Resources\Resource;
+use Filament\Resources\Table;
+use Filament\Tables;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CompanyInvitationResource extends Resource
+{
+    protected static ?string $model = CompanyInvitation::class;
+
+
+    protected static ?string $navigationIcon = 'heroicon-o-mail-open';
+
+    protected static ?string $navigationGroup = 'company';
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('company.name'),
+                Tables\Columns\TextColumn::make('email'),
+                Tables\Columns\TextColumn::make('role'),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime(),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\DeleteBulkAction::make(),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListCompanyInvitations::route('/'),
+            'create' => Pages\CreateCompanyInvitation::route('/create'),
+            'edit' => Pages\EditCompanyInvitation::route('/{record}/edit'),
+        ];
+    }
+}
