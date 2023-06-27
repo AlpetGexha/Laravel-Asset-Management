23bfb63a6815142293448e131523dd9c00081384
Alpet Gexha <50520333+AlpetGexha@users.noreply.github.com>
2023-06-27 18:03:07 +0200

    Add a policy

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -15,8 +15,10 @@ use App\Policies\CompanyPolicyMain;
 use App\Policies\HardwarePolicy;
 use App\Policies\PeriphelPolicy;
 use App\Policies\ProvaiderPolicy;
+use App\Policies\SchedulePolicy;
 use App\Policies\SoftwarePolicy;
 use App\Policies\UserPolicy;
+use HusamTariq\FilamentDatabaseSchedule\Models\Schedule;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
 use Spatie\Activitylog\Models\Activity;
 
@@ -28,13 +30,14 @@ class AuthServiceProvider extends ServiceProvider
      * @var array<class-string, class-string>
      */
     protected $policies = [
-        CompanyPolicyMain::class => Company::class,
-        HardwarePolicy::class => Hardware::class,
-        PeriphelPolicy::class => Periphel::class,
-        ProvaiderPolicy::class => Provaider::class,
-        SoftwarePolicy::class => Software::class,
+        Company::class => CompanyPolicyMain::class,
+        Hardware::class => HardwarePolicy::class,
+        Periphel::class => PeriphelPolicy::class,
+        Provaider::class => ProvaiderPolicy::class,
+        Software::class => SoftwarePolicy::class,
         Activity::class => ActivityPolicy::class,
         User::class => UserPolicy::class,
+        Schedule::class => SchedulePolicy::class,
     ];
 
     /**
